3b9892388fe62d70079d7c40fd721001065a693a
Víctor Calzón <kung.funk@gmail.com>
2016-08-06 22:26:09 +0200

    Added Articulos model. Added settings for CORS in .htaccess. Minor fixes in Usuarios model.

---
+++ src/Database/PDO/Repositories/ArticuloRepository.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Victor
+ * Date: 05/08/2016
+ * Time: 18:33
+ */
---
+++ src/Domain/Articulo/ArticuloFactory.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Victor
+ * Date: 06/08/2016
+ * Time: 21:40
+ */
